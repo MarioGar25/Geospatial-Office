3e3a06c77deea1966d85d942d93352a717bc3850
Mario <mariogarcia2506@gmail.com>
2021-02-09 10:30:04 +0100

    modificacion

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from functools import reduce
 import operator
-
+import requests
+import os
+import json
+from dotenv import load_dotenv
 
 def extract_city(dic):    
     ''' extracts the city from a dictionary '''
@@ -22,7 +25,7 @@ def get_coordinates(dic):
             "coordinates":[float(dic["longt"]),float(dic["latt"])]}
 
 
-def get_school_location_from_dic(dic,x):
+def get_location_from_dic(dic,x):
     ''' get values from dictionaries'''
     return reduce (operator.getitem,x,dic)
 
@@ -57,3 +60,69 @@ def get_points_schools(lst):
         return +2
     else:
         return +0
+
+def get_location_from_foursquare_in_SanFrancisco(establishment):
+    load_dotenv()
+    url = "https://api.foursquare.com/v2/venues/explore"
+    tok1 = os.getenv("token1")
+    tok2 = os.getenv("token2")
+    parameters = {"client_id":tok1,
+            "client_secret":tok2,
+            "v": "20180323",
+            "ll": "37.66881, -122.40443",
+              "query":establishment,
+              "limit": 100   }
+    response = requests.get(url=url, params = parameters).json()
+    print("Calling the Api")
+    data_json = response.get("response")
+
+    data_groups = data_json.get("groups")[0]
+
+    data_items = data_groups.get("items")
+    print("Getting the coordinates")
+    final_list = []
+    for dic in data_items:
+        final_dic = {}
+        i = dic.get("venue")
+        name = i.get("name")
+        location = i.get("location")
+        latitude = location.get("lat")
+        longitude = location.get("lng")
+        final_dic["name"] = name
+        final_dic["latitude"] = latitude
+        final_dic["longitude"] = longitude
+        final_list.append(final_dic)
+    print("Finished")
+    return final_list
+
+def get_location_from_foursquare_in_SanFrancisco_second_mod(establishment):
+    load_dotenv()
+    url = "https://api.foursquare.com/v2/venues/explore"
+    tok1 = os.getenv("token1")
+    tok2 = os.getenv("token2")
+    parameters = {"client_id":tok1,
+            "client_secret":tok2,
+            "v": "20180323",
+            "ll": "37.66881, -122.40443",
+              "query":establishment,
+              "limit": 100   }
+    response = requests.get(url=url, params = parameters).json()
+    print("Calling the Api")
+    data_json = response.get("response")
+
+    data_groups = data_json.get("groups")[0]
+
+    data_items = data_groups.get("items")
+    print("Getting the coordinates")
+    data_name = ["venue", "name"]
+    data_longitude = ["venue", "location", "lng"]
+    data_latitude = ["venue", "location", "lat"]
+    final_list = []
+    for dic in data_items:
+        final_dic = {}
+        final_dic["name"] = get_location_from_dic(dic, data_name)
+        final_dic["latitude"] = get_location_from_dic(dic, data_latitude)
+        final_dic["longitude"] = get_location_from_dic(dic, data_longitude)
+        final_list.append(final_dic)
+    
+    return final_list
